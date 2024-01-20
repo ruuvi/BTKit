a71f4306ae6df4ec54b208288a69fb86cb0d677c
Rinat Enikeev <rinat.enikeev@gmail.com>
2024-01-20 22:02:19 +0200

    DoCC with HelloRuuvi tutorial (#8)
    Easy to follow tutorial on how to use BTKit

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,11 +6,14 @@ import PackageDescription
 let package = Package(
     name: "BTKit",
     defaultLocalization: "en",
-    platforms: [.macOS(.v10_15), .iOS(.v10)],
+    platforms: [.macOS(.v10_15), .iOS(.v12)],
     products: [
         .library(
             name: "BTKit",
             targets: ["BTKit"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
