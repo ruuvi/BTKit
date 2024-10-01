fb81ccc84018984eb3df49064237a3b0fdfe2870
Priyonto M Rahman <priyontoo@gmail.com>
2024-10-01 19:19:48 +0200

    fixup

--- Sources/BTKit/Vendor/Ruuvi/Decoder/iOS/RuuviDecoderiOS.swift
+++ Sources/BTKit/Vendor/Ruuvi/Decoder/iOS/RuuviDecoderiOS.swift
@@ -99,7 +99,7 @@ public struct RuuviDecoderiOS: BTDecoder {
                 let ruuvi = manufacturerData.ruuvi5()
                 let tag = RuuviData5(uuid: uuid, rssi: rssi.intValue, isConnectable: isConnectable, version: Int(version), humidity: ruuvi.humidity, temperature: ruuvi.temperature, pressure: ruuvi.pressure, accelerationX: ruuvi.accelerationX, accelerationY: ruuvi.accelerationY, accelerationZ: ruuvi.accelerationZ, voltage: ruuvi.voltage, movementCounter: ruuvi.movementCounter, measurementSequenceNumber: ruuvi.measurementSequenceNumber, txPower: ruuvi.txPower, mac: ruuvi.mac)
                 return .ruuvi(.tag(.v5(tag)))
-            case 6:
+            case 254:
                 // Legacy
                 print("DEKHI VERSION: ", version, "\n")
 //                print("DEKHI 5: ", manufacturerData.ruuvi5(), "\n")
